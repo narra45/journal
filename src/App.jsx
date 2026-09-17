7f5be60f548e3a3e3088e9a2561bf1038efeba63
narra <narrasrikanth33@gmail.com>
2026-09-17 11:04:16 +0530

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './components/Login'
 import Register from './components/Register'
@@ -19,7 +19,7 @@ function App() {
   return (
     <div>
 
-         <BrowserRouter>
+         <HashRouter>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/login" element={<Login/>}/>
@@ -34,7 +34,7 @@ function App() {
 
 
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
 
 
        
