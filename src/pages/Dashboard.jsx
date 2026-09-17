c4b4f46052340ab3b83193dda316ebd99b912c3b
narra <narrasrikanth33@gmail.com>
2026-09-17 10:11:32 +0530

    changed

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import AddJournal from '../components/AddJournal'
 
-const API_BASE = 'http://localhost:8080/api'
+const API_BASE = 'https://journal-hbp1.onrender.com/api'
 
 export default function Dashboard() {
   const [entries, setEntries] = useState([])
